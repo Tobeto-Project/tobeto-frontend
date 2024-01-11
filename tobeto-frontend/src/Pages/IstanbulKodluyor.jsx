bbb8a3983e3fb289404f84f323f5deec06d03e4f
mac <Hasancanmidi@hotmail.com>
2024-01-12 00:05:41 +0300

    istanbulkodluyor update

--- tobeto-frontend/src/Pages/IstanbulKodluyor.jsx
+++ tobeto-frontend/src/Pages/IstanbulKodluyor.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from '../Assets/Images/istanbulkodluyor-logo-white.svg';
 import { Container, Row, Col, Image, Form, Button } from 'react-bootstrap';
+import  '../Styles/PagesStyles/IstanbulKodluyor.css'
 
 const IstanbulKodluyor = () => {
   return (
@@ -57,8 +58,11 @@ const IstanbulKodluyor = () => {
                             {/* İçerik */}
                         </div>
                         <ul className="text-black list-unstyled ms-5 fs-4 mt-5">
-                        <li>18-35 yaş arasındaysan</li>
-                        <li>Lise veya üniversite mezunuysan</li>
+                        <li><span className="text-black fw-bold">1</span> Sıkça Sorulan Sorulara Bakabilirsin</li>
+                        <Button className="rounded-pill btn-dark">S.S.S</Button>
+                        <li className="mt-4"><span className="text-black fw-bold">2</span> Bize Yazabilirsin</li>
+                        <Button className="rounded-pill btn-dark">Message</Button>
+
                     </ul>
                     </div>
                 </Col>
@@ -80,17 +84,56 @@ const IstanbulKodluyor = () => {
                         <Form.Group className="mb-3" controlId="formPasswordAgain">
                             <Form.Control type="password" placeholder="Şifre Tekrar*" />
                         </Form.Group>
-                        <Button variant="primary" type="submit" className="w-100" style={{ marginTop: '24px' }}>
+                        <Button variant="primary" type="submit" className="w-100 rounded-pill" style={{ marginTop: '24px' , backgroundColor:'#BF7FFC'}}>
                             Kayıt Ol
                         </Button>
                     </Form>
                     <div style={{ marginTop: '24px' }}>
-                        <label>
+                        <label className="d-flex justify-content-center">
                             <small>Zaten bir hesabın var mı?<a className="text-decoration-none text-muted fw-bold" href="giris.html"> Giriş Yap</a></small>
                         </label>
                     </div>
                 </Col>
             </Row>
+            <Row style={{ backgroundColor: '#00D29B' }}> {/* bg-lightgreen yerine inline CSS */}
+                <Col xs={12} className="header-left-2 mt-3" style={{  }}>
+                    <span className="header-left-text text-white" style={{ backgroundColor:"#100A3A"}}>
+                        İstanbul Kodluyor Projesi Hakkında
+                    </span>
+                </Col>
+                <Col xs={12} style={{ marginTop: '32px' }}> {/* mt-8 yerine inline CSS */}
+                  <Container>
+                    <p className="text-white fs-4">
+                        <b>Yazılım ve teknoloji</b> alanında meslek sahibi olmak isteyen genç yetişkinlere, <b>ücretsiz
+                        ve kapsamlı eğitimlerle</b> gerekli bilgi ve becerileri kazandıran, Türkiye’nin önde gelen
+                        kurumlarında <b>işe yerleşmelerini</b> sağlayan bir projedir. Teknoloji alanında nitelikli insan
+                        kaynağını geliştirmeyi amaçlayan bu proje, aynı zamanda <b>kadınların</b> sektördeki
+                        temsiliyetini artırmayı da hedeflemektedir.
+                    </p>
+                    </Container>
+                </Col>
+            </Row>
+            <Container fluid>
+            <Row style={{ backgroundColor: '#1E1041', padding: '10px' }}> {/* bg-darkblue ve p-10 için inline CSS */}
+                <Col xs={12} className="text-center">
+                    <span className="text-neon" style={{ /* text-neon için gerekli stiller */ }}>SÜREÇ</span>
+                    <section className="ps-timeline-sec" style={{ /* ps-timeline-sec için gerekli stiller */ }}>
+                        <ol className="ps-timeline" style={{ /* ps-timeline için gerekli stiller */ }}>
+                            {/* Süreç adımları */}
+                            <li>
+                                <div className="ps-top" style={{ /* ps-top için gerekli stiller */ }}>
+                                    <p>Başvuru ve <br /> Ön-değerlendirme</p>
+                                </div>
+                                <span className="ps-sp-top" style={{}}>1</span>
+                            </li>
+                        </ol>
+                    </section>
+                </Col>
+            </Row>
+        </Container>
+ 
+            
+ 
     </>
   );
 };
