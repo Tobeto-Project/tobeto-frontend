d76528670ca8e1aacbb88714daa4ef62bd65a83e
mac <Hasancanmidi@hotmail.com>
2024-02-01 17:53:52 +0300

    Homepage finish

--- tobeto-frontend/src/Components/Common/HomePage/HomepageCards.jsx
+++ tobeto-frontend/src/Components/Common/HomePage/HomepageCards.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import link from "/Users/mac/Desktop/Github's/react-pair1/tobeto-frontend/src/Assets/Images/anasayfa-left-first.svg"
 import link2 from "/Users/mac/Desktop/Github's/react-pair1/tobeto-frontend/src/Assets/Images/anasayfa-right.svg";
 import link3 from "/Users/mac/Desktop/Github's/react-pair1/tobeto-frontend/src/Assets/Images/anasayfa-left-inside.png";
-import link4 from "/Users/mac/Desktop/Github's/react-pair1/tobeto-frontend/src/Assets/Images/istanbulkodluyor-black.svg";
-import { Col, Image, Row } from "react-bootstrap";
+import link4 from "/Users/mac/Desktop/Github's/react-pair1/tobeto-frontend/src/Assets/Images/istanbulkodluyor-logo-white.svg";
+import { Button, Col, Image, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const HomepageCards = () => {
   return (
@@ -12,26 +13,29 @@ const HomepageCards = () => {
         <Col md={6} className="image-container">
           <Image src={link} fluid />
           <div className="overlay">
-            <p>
-              Hayalindeki teknoloji kariyerini başlat! İstediğin yoldan hızda
+            <p className="ms-4 fw-bold fs-3">
+              Hayalindeki teknoloji <br /> kariyerini başlat! <br />İstediğin yoldan <span className="px-2 rounded-pill" style={{backgroundColor:"#9927E7"}}>hızda</span>             
+              <Link to={"/uyeol"}><Button className="p-1" style={{margin:"0"}}>Ücretsiz Üye Ol</Button></Link>
             </p>
+
             <img
-             className=""
+             className="me-4"
               src={link3}
               alt="Ek Görsel"
-              style={{ height: "150px", width: "150px" }}
+            
             />
           </div>
         </Col>
         <Col md={6} className="image-container">
           <Image src={link2} fluid className="rounded" />
-          <div className="overlay">
-            <p>Aradığın " İş " Burada!</p>
+          <div className="overlay2">
+            <div className="overlay2-box"></div><p className="fw-bold">Aradığın <span style={{color:"#00B078"}}>"</span> İş <span style={{color:"#00B078"}}>"</span> Burada!</p>
             <img
+            className="me-4"
               src={link4}
               alt="Ek Görsel"
-              style={{ height: "150px", width: "150px" }}
             />
+            <Link to={"/IstanbulKodluyor"}><Button className="p-1 overlay-button" style={{margin:"0"}}>Başvur</Button></Link>
           </div>
         </Col>
       </Row>
