36ae9e5eae01204cce8ffee53e8bc67417acc33c
Gülüzar Karakoç <karakoc.guluzar99@gmail.com>
2024-02-03 11:50:32 +0300

    Blog

--- tobeto-frontend/src/Pages/Blog.jsx
+++ tobeto-frontend/src/Pages/Blog.jsx
@@ -2,20 +2,65 @@ import React from "react";
 import Banner from "../Components/Layouts/Banner";
 import Header from "../Components/Layouts/Header";
 import Footer from "../Components/Layouts/Footer";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/esm/Col";
+import Card from 'react-bootstrap/Card';
+import Container from "react-bootstrap/esm/Container";
 
 const Blog = () => {
   return (
     <div className="body-container pages-content">
       <Banner />
       <Header />
-
-      <div className="main-content">
-        <h1>Blog</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas totam nesciunt commodi consectetur impedit dolore? Soluta suscipit, dolores aliquid, voluptates expedita adipisci ea illum sapiente cupiditate numquam necessitatibus aspernatur architecto incidunt at ullam consectetur asperiores consequuntur! Inventore, tempora suscipit? Corrupti natus, doloribus dolorum fuga eius nulla veritatis! Eius hic sapiente mollitia nobis eaque officia asperiores consequatur quos iure, quae voluptatibus molestiae optio, nostrum vero facilis error blanditiis. Impedit quae numquam quidem, nemo magni illum consequuntur consequatur suscipit harum a sunt cupiditate fugiat deserunt dolores odio natus at nesciunt similique quo modi magnam mollitia itaque! Ad sunt explicabo ullam perferendis omnis, quasi ratione in, laudantium soluta delectus quibusdam, dicta aliquid voluptate hic odio expedita quaerat fuga et perspiciatis magnam nobis conseq
-        </p>
+      <div className="container" style={{ 'padding-top': '12em', 'margin-bottom': '1em', 'width': 'max-content' }}>
+        <div className="row text-center">
+          <h1 className="ms-4">Blog</h1>
+        </div>
       </div>
+      <Container>
 
+        <Row>
+          <Col md={4}>
+            <div style={{ padding: 4 }}>
+              <a className="blog-card" href="">
+                <Card style={{
+                  width: '350px', height: '450px',
+                  background: `url("https://images.nightcafe.studio//assets/tdraw-girl.jpg?tr=w-1200,c-at_max")`, backgroundSize: 'cover'
+                }}>
+                  <Card.Body>
+                    <div class="date" style={{ position: 'absolute', top: '0', right: '0', opacity: '0.8', padding: '1em', color: 'white' }}>2 Şubat 2024</div>
+                    <div className="d-flex " style={{ flexDirection: 'column', justifyContent: 'flex-end',alignContent:'flex-end'}}>
+                      <Card.Title style={{ color: '#FFFFFF' }}>Card Title</Card.Title>
+                      <Card.Text className="description" style={{ color: '#FFFFFF' }}>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Officia minus tenetur temporibus, assumenda sint voluptates?
+                      </Card.Text>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </a>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div style={{ padding: 4 }}>
+              <a className="blog-card" href="">
+                <Card style={{
+                  width: '350px', height: '450px',
+                  background: `url("https://images.nightcafe.studio/jobs/K8WrdX7EOvXMP2Hu8QBR/K8WrdX7EOvXMP2Hu8QBR--2--0xle7_12.5x.jpg?tr=w-1600,c-at_max")`, backgroundSize: 'cover'
+                }}>
+                  <Card.Body>
+                    <div class="date" style={{ position: 'absolute', top: '0', right: '0', opacity: '0.8', padding: '1em', color: 'white' }}>2 Şubat 2024</div>
+                    <Card.Title style={{ color: '#FFFFFF' }}>Card Title</Card.Title>
+                    <Card.Text className="description" style={{ color: '#FFFFFF' }}>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quidem eos repudiandae nihil nam vel.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </a>
+            </div>
+          </Col>
+          <Col md={4}></Col>
+        </Row>
+      </Container>
       <Footer />
     </div>
   );
