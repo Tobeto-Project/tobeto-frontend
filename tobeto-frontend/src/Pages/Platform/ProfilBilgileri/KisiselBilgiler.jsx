97fcee74b04757912e2b1bce7ff9227895abe103
mac <Hasancanmidi@hotmail.com>
2024-02-04 21:56:17 +0300

    Kisisel Bilgiler bazı düzenlemeler.

--- tobeto-frontend/src/Pages/Platform/ProfilBilgileri/KisiselBilgiler.jsx
+++ tobeto-frontend/src/Pages/Platform/ProfilBilgileri/KisiselBilgiler.jsx
@@ -9,6 +9,7 @@ import PlatformCertificates from '../../../Components/Layouts/PlatformCertificat
 import PlatformMediaAccounts from '../../../Components/Layouts/PlatformMediaAccounts';
 import { PlatformForeignLanguage } from '../../../Components/Layouts/PlatformForeignLanguage';
 import PlatformSettings from '../../../Components/Layouts/PlatformSettings';
+import '../../../Styles/PagesStyles/PlatformStyle/KisiselBilgilerStyle.css';
 
 function PlatformKisiselMenu() {
   const [activeTab, setActiveTab] = useState("kisiselbilgilerim");
@@ -36,27 +37,44 @@ function PlatformKisiselMenu() {
         return <Card.Body>İçerik bulunamadı.</Card.Body>;
     }
   };
+  const getTabStyle = (tabName) => {
+    if (activeTab === tabName) {
+      return { color: '#9833FF' };
+    } else {
+      return { color: '#828282' };
+    }
+  };
+  
 
   return (
     <>
     <PlatformHeader/>
     <Container className='my-5'>
       <Row>
-        {/* Sol Taraf: Dikey Menü */}
         <Col md={3}>
-          <Nav className="flex-column">
-            <Nav.Link onClick={() => setActiveTab("kisiselbilgilerim")}>Kişisel Bilgilerim</Nav.Link>
-            <Nav.Link onClick={() => setActiveTab("deneyimlerim")}>Deneyimlerim</Nav.Link>
-            <Nav.Link onClick={() => setActiveTab("egitimhayatim")}>Eğitim Hayatım</Nav.Link>
-            <Nav.Link onClick={() => setActiveTab("yetkinliklerim")}>Yetkinliklerim</Nav.Link>
-            <Nav.Link onClick={() => setActiveTab("sertifikalarım")}>Sertifikalarım</Nav.Link>
-            <Nav.Link onClick={() => setActiveTab("medya")}>Medya Hesaplarım</Nav.Link>
-            <Nav.Link onClick={() => setActiveTab("yabancı dillerim")}>Yabancı Dillerim</Nav.Link>
-            <Nav.Link onClick={() => setActiveTab("ayarlar")}>Ayarlar</Nav.Link>
+          <Nav className="flex-column ">
+            <Nav.Link 
+            className='fw-bold' 
+            style={getTabStyle("kisiselbilgilerim")} 
+            onClick={() => setActiveTab("kisiselbilgilerim")}>
+            <img className='me-2 mb-1' src='https://tobeto.com/user.svg' style={getTabStyle("kisiselbilgilerim")}></img>Kişisel Bilgilerim</Nav.Link>
+
+            <Nav.Link className='fw-bold mt-2'  style={getTabStyle("deneyimlerim")} onClick={() => setActiveTab("deneyimlerim")}><img className='me-2 mb-1' src='https://tobeto.com/business.svg' style={getTabStyle("deneyimlerim")}></img>Deneyimlerim</Nav.Link>
+
+            <Nav.Link className='fw-bold  mt-2'  style={getTabStyle("egitimhayatim")} onClick={() => setActiveTab("egitimhayatim")}><img className='me-2 mb-1' src='https://tobeto.com/book.svg' style={getTabStyle("egitimhayatim")}></img>Eğitim Hayatım</Nav.Link>
+
+            <Nav.Link className='fw-bold  mt-2'  style={getTabStyle("yetkinliklerim")} onClick={() => setActiveTab("yetkinliklerim")}><img className='me-2 mb-1' src='https://tobeto.com/award.svg' style={getTabStyle("yetkinliklerim")}></img>Yetkinliklerim</Nav.Link>
+
+            <Nav.Link className='fw-bold  mt-2'  style={getTabStyle("sertifikalarım")} onClick={() => setActiveTab("sertifikalarım")}><img className='me-2 mb-1' src='https://tobeto.com/certificates.svg' style={getTabStyle("sertifikalarım")}></img>Sertifikalarım</Nav.Link>
+
+            <Nav.Link className='fw-bold  mt-2'  style={getTabStyle("medya")} onClick={() => setActiveTab("medya")}><img className='me-2 mb-1' src='https://tobeto.com/globe.svg' style={getTabStyle("medya")}></img>Medya Hesaplarım</Nav.Link>
+
+            <Nav.Link className='fw-bold  mt-2'  style={getTabStyle("yabancı dillerim")} onClick={() => setActiveTab("yabancı dillerim")}><img className='me-2 mb-1' src='https://tobeto.com/translate.svg' style={getTabStyle("yabancı dillerim")}></img>Yabancı Dillerim</Nav.Link>
+
+            <Nav.Link className='fw-bold  mt-2'  style={getTabStyle("ayarlar")} onClick={() => setActiveTab("ayarlar")}><img className='me-2 mb-1' src='https://tobeto.com/settings.svg' style={getTabStyle("ayarlar")}></img>Ayarlar</Nav.Link>
           </Nav>
         </Col>
 
-        {/* Sağ Taraf: İçerik Alanı */}
         <Col md={9}>
           <Card>
             {renderContent()}
@@ -68,4 +86,4 @@ function PlatformKisiselMenu() {
   );
 }
 
-export default PlatformKisiselMenu
+export default PlatformKisiselMenu;
