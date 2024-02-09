5a1824a281154703c7e8ca327530b5d507b1e538
mac <Hasancanmidi@hotmail.com>
2024-02-09 21:31:25 +0300

    Admin panelinden blog ekleme işlemi tamamlandı.

---
+++ tobeto-frontend/src/AdminPanel/pages/blog/BlogForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Form, Button, Container } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; 
+
+function BlogForm() {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const blogData = { title, text };
+    // API'ye POST isteği gönder
+    try {
+      const response = await fetch('http://localhost:5082/api/Blogs/add', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(blogData),
+      });
+      if (response.ok) {
+        toast.success('Blog başarıyla eklendi');
+      } else {
+        toast.error('Blog eklenirken bir hata oluştu');
+      }
+    } catch (error) {
+      console.error('Blog eklenirken bir hata oluştu', error);
+    }
+  };
+
+  return (
+    <Container className='mt-5'>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="formBlogTitle">
+          <Form.Label>Blog Başlığı</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Başlık girin"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBlogText">
+          <Form.Label>Blog İçeriği</Form.Label>
+          <ReactQuill rows={20} theme="snow" value={text} onChange={setText} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Blog Ekle
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default BlogForm;
---
+++ tobeto-frontend/src/Pages/BlogIcerik.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Banner from '../Components/Layouts/Banner';
+import Header from '../Components/Layouts/Header';
+import Footer from '../Components/Layouts/Footer';
+import { Col, Container, Row } from 'react-bootstrap';
+
+const BlogIcerik = () => {
+    const { blogId } = useParams();
+    const [blog, setBlog] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState('');
+  
+    useEffect(() => {
+      const fetchBlogDetail = async () => {
+        try {
+          const response = await fetch(`http://localhost:5082/api/Blogs/${blogId}`);
+          if (!response.ok) {
+            throw new Error('Blog yüklenirken bir hata oluştu.');
+          }
+          const data = await response.json();
+          setBlog(data);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+  
+      fetchBlogDetail();
+    }, [blogId]);
+  
+    if (isLoading) {
+      return <div>Yükleniyor...</div>;
+    }
+  
+    if (error) {
+      return <div>Hata: {error}</div>; // Hata mesajını burada göster
+    }
+  return (
+    <>
+      <div className="body-container pages-content">
+        <Banner />
+        <Header />
+
+        <Container style={{ paddingTop: "12em", marginBottom: "1em", maxWidth: "720px" }}>
+          <Row className="text-center">
+            <Col>
+            <h1>{blog.title}</h1>
+            </Col>
+          </Row>
+          <Row>
+          <Col md={12}>
+            <div dangerouslySetInnerHTML={{ __html: blog.text }} />
+            </Col>
+          </Row>
+        </Container>
+
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default BlogIcerik;
