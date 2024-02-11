b1b59a388948f1131df7601064bf153220dc581c
hasankesil <hasankesil@hotmail.com>
2024-02-11 22:56:00 +0300

    KatalogCard and KatalogMenu modified, Accourdion sidebar added

--- tobeto-frontend/src/Components/Common/KatalogCard.jsx
+++ tobeto-frontend/src/Components/Common/KatalogCard.jsx
@@ -1,45 +1,92 @@
 import React, { useState, useEffect } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Row, Col } from "react-bootstrap";
+import { BsPeople, BsClock } from "react-icons/bs"; // Eklediğimiz ikonlar
 import "../../Styles/CommonStyles/KatalogCardStyle.css";
 import { getCourseData } from "../../Services/CourseService";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const KatalogCard = () => {
-    const [courseList, setCourseList] = useState([]);
-    const { isLoggedIn } = useSelector(state => state.auth);
-    const navigate = useNavigate();
+  const [courseList, setCourseList] = useState([]);
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
 
-    const handleCardClick = () => {
-      if(isLoggedIn){
-        navigate('/kurs');
-      }else{
-        navigate('/girisyap')
-      }
+  const handleCardClick = () => {
+    if (isLoggedIn) {
+      navigate("/kurs");
+    } else {
+      navigate("/girisyap");
     }
+  };
 
-    useEffect(() => {
-        getCourseData().then(data =>{
-            setCourseList(data);
-        });
-    },[])
+  useEffect(() => {
+    getCourseData().then((data) => {
+      setCourseList(data);
+    });
+  }, []);
 
-return (
+  return (
     <>
-    <div className="card-container">
-      {courseList.map((data) => (
-        <Card className="education-card" key={data.id} onClick={handleCardClick}>
-          <Card.Img variant="top" src={data.image} />
-          <div className="card-info">
-            <div className="card-text">
-              <h5>{data.teacher}</h5>
-              <p>{data.duration}</p>
-            </div>
-            <h4>{data.subject}</h4>
-          </div>
-        </Card>
-      ))}
-    </div>
+      <div className="card-container">
+        <Row xs={1} md={2} lg={3} xl={3} className="g-4">
+          {courseList.map((data) => (
+            <Col key={data.id}>
+              <Card
+                className="education-card"
+                onClick={handleCardClick}
+                style={{ position: "relative", width: "90%" }}
+              >
+                <Card.Img variant="top" src={data.image} />
+                <div className="card-info">
+                  <Row>
+                    <Col xs={12} md={6}>
+                      <div className="card-text">
+                        <h5 style={{ fontSize: "1rem" }}>{data.teacher}</h5>
+                      </div>
+                    </Col>
+                    <Col xs={12} md={6}>
+                      <div className="card-text">
+                        <p style={{ fontSize: "1rem" }}>{data.duration}</p>
+                      </div>
+                    </Col>
+                  </Row>
+                  <Row>
+                  
+                      <h4 style={{ fontSize: "1rem" }}>{data.subject}</h4>
+                   
+                  </Row>
+                </div>
+                {/* Yeni eklenen bölüm */}
+                <div
+                  style={{
+                    position: "absolute",
+                    bottom: "0",
+                    left: "0",
+                    width: "100%",
+                    backgroundColor: "#f8f9fa",
+                    padding: "10px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    height: "70px",  // Sabit yükseklik değeri
+                  }}
+                >
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <BsPeople style={{ marginRight: "0.5rem" }} />
+                    <span>{data.teacher}</span>
+                  </div>
+                  <div style={{ display: "flex", alignItems: "center" }}>
+                    <BsClock style={{ marginRight: "0.5rem" }} />
+                    <span>{data.duration}</span>
+                  </div>
+                  <div style={{ textAlign: "center" }}>{data.subject}</div>
+                </div>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </div>
     </>
   );
 };
--- tobeto-frontend/src/Components/Common/MiddleBanner.jsx
+++ tobeto-frontend/src/Components/Common/MiddleBanner.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "../../Styles/CommonStyles/MiddleBannerStyle.css";
+import SearchComponent from './PlatformKatalog/SearchComponent';
 
 const MiddleBanner = ({ children }) => {
   return (
-    <div className='middle-banner'>
-        {children}
+    <div className='middle-banner p-1'>
+      {children}
+      <SearchComponent />
     </div>
   );
 }
