1883d2f051d606505b0210c236724b0998960499
hasankesil <hasankesil@hotmail.com>
2024-02-18 15:33:23 +0300

    Header and PlatformHeader's responsive toggle icon added, Services configurated

--- tobeto-frontend/src/Services/NotificationService.js
+++ tobeto-frontend/src/Services/NotificationService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
+import API_CONFIG from './ApiConfig';
+
 
 const getNotifications = (pageIndex = 0, pageSize = 50) => {
-    return axios.get(`http://localhost:5082/api/Notifications/getList`, {
+    return axios.get(`${API_CONFIG.NOTIFICATIONS_GET_LIST}`, {
         params: {
             PageIndex: pageIndex,
             PageSize: pageSize
