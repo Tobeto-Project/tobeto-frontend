a827ca202893586a01da49872738937dbc64f588
fatihdemirag <f2000fatih@hotmail.com>
2024-02-27 21:26:21 +0300

    canlı bağlantısı eklendi

--- tobeto-frontend/src/Services/config.js
+++ tobeto-frontend/src/Services/config.js
@@ -3,5 +3,6 @@
 const PORT = 5082; //swagger browser port number
 
 const API_URL = `http://localhost:${PORT}/api`;
+//const API_URL = `https://www.api.tobeto1cpair1.com.tr/api`;
 
 export default API_URL;
