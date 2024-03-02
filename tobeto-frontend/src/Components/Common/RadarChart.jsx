0db6c5ad215c0d7a5ed8b18116cfbee8daa30d3b
Gülüzar Karakoç <karakoc.guluzar99@gmail.com>
2024-03-02 20:29:25 +0300

    Profilim-RadarChart

--- tobeto-frontend/src/Components/Common/RadarChart.jsx
+++ tobeto-frontend/src/Components/Common/RadarChart.jsx
@@ -5,15 +5,33 @@ import Chart from 'chart.js/auto';
 
 
 const RadarChart = () => {
+  const originalData = [5, 4, 2.5, 5, 4, 4.5, 5, 4];
   const data = {
-    labels: ['', '', '', '', '', ''],
+    labels: ['', '', '', '', '', '', '',''],
     datasets: [
       {
         label: 'Kişisel İstatistikler',
-        data: [5,5,5,5,5,5,5,5],
-        backgroundColor: 'rgba(34, 202, 236, 0.2)',
-        borderColor: 'rgba(34, 202, 236, 1)',
-        borderWidth: 1,
+        data: [...originalData, originalData[0]], // Veri noktalarını çiftleyerek çember gibi birleştir
+        backgroundColor: [
+          'rgba(34, 202, 236, 1)',
+          'rgba(255, 99, 132, 1)', 
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 205, 86, 1)', 
+          'rgba(153, 102, 255, 1)', 
+          'rgba(255, 159, 64, 1)', 
+        ],
+        borderColor: [
+          'rgba(34, 202, 236, 1)',
+          'rgba(255, 99, 132, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(255, 205, 86, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 0.5,
+        pointRadius: 5, 
+        pointHoverRadius: 8,
+        fill: true,
       },
     ],
   };
